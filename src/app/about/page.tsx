8cb9b59964c42f964662e1cc74da98c6836f0f6c
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-03-27 14:40:09 +0300

    made changes to the layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <main className="p-5">
+        <h1 className="text-4xl font-bold">This is the about page</h1>
+      </main>
+    );
+  }
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 mdLpx-5`}
       >
-        {children}
+   <header className="text-white font-bold bg-green-900 text-2xl">
+       <div className="flex flex-grow">
+           <Link href="/">GPT chat</Link>
+           <Link href="/about" className="ml-5 font-light">
+               About
+           </Link>
+       </div>
+   </header>
+
+   <div className="flex flex col md:flex-row">
+       <div className="flex-grow">
+           {children}
+       </div>
+   </div>
+
       </body>
     </html>
   );
