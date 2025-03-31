cb17ed495ac26bdbe90d6153695c23889b4425be
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-03-31 16:53:21 +0300

    Cretae PreviousChat and Transcript Components to show the prevuious chats on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
 import Chat from "@/app/components/Chat";
-import {Separator} from "@/components/ui/separator";
-import {auth} from "@/auth";
+import { Separator } from "@/components/ui/separator";
+import { auth } from "@/auth";
+import PreviousChats from "@/app/components/PreviousChat";
+import { Suspense } from "react";
 
-
-export default  async function Home() {
-
-
-    const session = await auth()
-    console.log(session)
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
 
-        {!session?.user?.email && <div>You need to be logged in to use this chat</div>}
+      {!session?.user?.email && (
+        <div>You need to be logged in to use this chat</div>
+      )}
 
+      {session?.user?.email && (
+        <>
+          <Suspense fallback={<div> Loading Previous Chats ...</div>}>
+            <PreviousChats />
+          </Suspense>
 
-        {session?.user?.email &&  <>
-            <Separator className="my-5"/>
-            <Chat/>
-        </>}
-
-
+          <Separator className="my-5" />
+          <h4 className="mt-5 text-2xl font-bold">New Chat Session</h4>
+          <Chat />
+        </>
+      )}
     </main>
   );
 }
